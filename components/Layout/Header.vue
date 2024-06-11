4e7522b04586c8f4f840067af8d4e10cec91b3c9
Rian Gomes <rianluccas08@gmail.com>
2024-06-10 22:12:59 -0300

    feat: add border on click in nav link

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -1,21 +1,23 @@
 <template>
-  <div>
-    <header class="flex items-center justify-between bg-defaultBgColor border-b border-defaultFontColor">
-      <div class="flex">
-        <SpecificHeaderButton name="_rian-lucas" link="/" size="large" />
-        <SpecificHeaderButton name="_about-me" link="/about-me" />
-        <SpecificHeaderButton name="_projects" link="/projects" />
-        <SpecificHeaderButton name="_blog" link="/blog"/>
-      </div>
-      <SpecificHeaderButton name="contact-me" link="/contact-me" />
-    </header>
-  </div>
+    <div>
+        <header
+            class="flex items-center justify-between bg-defaultBgColor border-b border-defaultFontColor"
+        >
+            <div class="flex">
+                <SpecificHeaderButton
+                    name="_rian-lucas"
+                    link="/"
+                    size="large"
+                />
+                <SpecificHeaderButton name="_about-me" link="#" />
+                <SpecificHeaderButton name="_projects" link="#" />
+                <SpecificHeaderButton name="_blog" link="#" />
+            </div>
+            <SpecificHeaderButton name="contact-me" link="/contact-me" />
+        </header>
+    </div>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" setup></script>
 
-</script>
-
-<style>
-
-</style>
+<style></style>
--- components/Specific/HeaderButton.vue
+++ components/Specific/HeaderButton.vue
@@ -1,45 +1,58 @@
 <template>
-  <div class="bg-defaultBgColor">
-    <NuxtLink
-      :class="[
-        'bg-defaultBgColor text-defaultFontColor border-r border-defaultBorderColor border-b-2 border-b-transparent flex items-center hover:text-white hover:border-b-2 hover:border-b-orangeAcentColor hover:transition-all',
-        paddingClass
-      ]"
-      :to="link"
-    >
-      <span>{{ name }}</span>
-    </NuxtLink>
-  </div>
+    <div class="bg-defaultBgColor">
+        <NuxtLink
+            id="nav-link"
+            @click="toggleFocus"
+            :class="[
+                'bg-defaultBgColor text-defaultFontColor border-r border-defaultBorderColor border-b-2 border-b-transparent flex items-center hover:text-white hover:border-b-2 hover:border-b-orangeAcentColor hover:transition-all',
+                paddingClass,
+            ]"
+            :to="link"
+        >
+            {{ name }}
+        </NuxtLink>
+    </div>
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue';
+import { computed } from "vue";
+
+function toggleFocus(event) {
+    document.querySelectorAll("#nav-link").forEach((link) => {
+        link.classList.remove("border-b-orangeAcentColor");
+        link.classList.remove("text-white");
+    });
+
+    event.target.classList.add("border-b-orangeAcentColor");
+    event.target.classList.add("text-white");
+}
 
 const props = defineProps({
-  name: {
-    type: String,
-    required: true
-  },
-  link: {
-    type: String,
-    required: true
-  },
-  size: {
-    type: String,
-    default: 'medium',
-    validator: (value: string) => ['small', 'medium', 'large'].includes(value)
-  }
+    name: {
+        type: String,
+        required: true,
+    },
+    link: {
+        type: String,
+        required: true,
+    },
+    size: {
+        type: String,
+        default: "medium",
+        validator: (value: string) =>
+            ["small", "medium", "large"].includes(value),
+    },
 });
 
 const paddingClass = computed(() => {
-  switch (props.size) {
-    case 'small':
-      return 'py-2 px-4';
-    case 'large':
-      return 'py-[0.71rem] px-32';
-    default:
-      return 'py-[0.71rem] px-8';
-  }
+    switch (props.size) {
+        case "small":
+            return "py-2 px-4";
+        case "large":
+            return "py-[0.71rem] px-32";
+        default:
+            return "py-[0.71rem] px-8";
+    }
 });
 </script>
 
